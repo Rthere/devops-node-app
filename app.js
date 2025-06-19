872fbb69b7cb907e17128bc4dc088f2efaaf1945
Rohan Tathe <rohantathe551@gmail.com>
2025-06-19 11:54:09 +0530

    Test CI/CD

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js DevOps Project!');
+  res.send('Deployed via GitHub Actions 🚀');
 });
 
 app.listen(port, () => {
